8f3fc59ffc7e85d8c29833a9e173b07e6283e68d
Harish <harishrd321@gmail.com>
2021-11-27 22:50:50 +0530

    +os,py

---
+++ 2-1/py-lab/program2b.py
@@ -0,0 +1,8 @@
+#python program for reading multiple inputs with comma
+R1, R2, R3, R4 = map(int, input("Enter four numbers").split(","))
+print("The value of R1 is ", R1)
+print("the value of R2 is ", R2)
+print("the value of R3 is ", R3)
+print("the value of R4 is ", R4)
+additon = R1 + R2 + R3 + R4
+print("the value of sum is", additon)
