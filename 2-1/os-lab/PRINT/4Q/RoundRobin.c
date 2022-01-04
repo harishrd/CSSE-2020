8b6169fda786ddcfc6cda35202124b6d215dc386
20121A1566@MNSDOMAIN.COM <20121A1566@MNSDOMAIN.COM>
2022-01-04 11:20:03 +0530

    os print

---
+++ 2-1/os-lab/PRINT/4Q/RoundRobin.c
@@ -0,0 +1,50 @@
+//Round Robin Algorithm (CMPD,ERR)
+#include<stdio.h>
+
+main() {
+   int bt[10], wt[10], tat[10], t[10], n, tq, i;
+   int count = 0, swt = 0, stat = 0, temp, sq = 0;
+   float awt, atat;
+   printf("\nEnter the no.of process: ");
+   scanf("%d", & n);
+   printf("\nEnter the burst time: ");
+   for (i = 0; i < n; i++) {
+      scanf("%d", & bt[i]);
+      t[i] = bt[i];
+   }
+   printf("\nEnter the time quantum: ");
+   scanf("%d", & tq);
+   while (1) {
+      for (i = 0; i < n; i++) {
+         temp = tq;
+         if (t[i] == 0) {
+            count++;
+            continue;
+         }
+         if (t[i] > tq) t[i] = t[i] - tq;
+         else if (t[i] >= 0) {
+            temp = t[i];
+            t[i] = 0;
+         }
+         sq = sq = temp;
+         tat[i] = sq;
+      }
+      if (n <= count)
+         break;
+   }
+   for (i = 0; i < n; i++) {
+      wt[i] = tat[i] - bt[i];
+      swt = swt + wt[i];
+      stat = stat + tat[i];
+   }
+   awt = (float) swt / n;
+   atat = (float) stat / n;
+   printf("\nThe process details: ");
+   printf("\n\n process number\t burst time\t waiting time\t turnaround time");
+   for (i = 0; i < n; i++) {
+      printf("\n%d\t\t%d\t\t%d\t\t%d", i + 1, bt[i], wt[i], tat[i]);
+   }
+   printf("\nAverage waiting time is: %f", awt);
+   printf("\nAverage turnaround time is: %f", atat);
+}
+
---
+++ 2-1/os-lab/PRINT/6Q/6_Bankers_Alg.c
@@ -0,0 +1,108 @@
+// 6. Bankers Algorithm (executed)
+
+#include <stdio.h>
+void main () {
+	int k=0, output[10],d=0,t=0,ins[5],i,avail[5];
+	int allocated[10][5],need[10][5],MAX[10][5],pno,P[10],rz,j,count=0;
+	
+	printf("\nEnter the no. of resources: ");
+	scanf("%d", &rz);
+	printf("\nEnter the max instances of each resources\n");
+	for(i=0; i<rz; i++) 
+	{
+		avail[i] == 0;
+		printf("%c = ", (i+97));
+		scanf("%d", &ins[i]);
+	}
+	printf("\nEnter no. of processes: ");
+	scanf("%d", &pno);
+	
+	printf("\nEnter the allocation matrix\n");
+	
+	for(i=0; i<rz; i++) 
+		printf("%c", (i+97));
+	printf("\n");
+	for(i=0; i<pno; i++) 
+	{
+		P[i] = i;
+		printf("P[%d]-", P[i]);
+		for(j=0; j<rz; j++) 
+		{
+			scanf("%d", &allocated[i][j]);
+			avail[j] += allocated[i][j];
+		}	
+	}
+	printf("\nEnter the MAX matrix\n");
+	for(i=0; i<rz; i++) 
+	{
+		printf("%c", (i+97));
+		avail[i] = ins[i] - avail[i];
+	}
+					
+	printf("\n");
+	for(i=0; i<pno; i++) 
+	{
+		printf("P[%d]-",i);
+		for(j=0; j<rz; j++) 
+			scanf("%d", &MAX[i][j]);
+	}
+	// PAGE 3
+	printf("\n");
+	A: d = -1;
+	for(i=0; i<pno; i++) 
+	{
+		count = 0, t=P[i];
+		for(j=0; j<rz; j++)
+		{
+			need[t][j] = MAX[t][j] - allocated[t][j];
+			if(need[t][j] <= avail[j])
+				count++;
+		}
+		if(count == rz) 
+		{
+			output[k++] = P[i];
+			for(j=0; j<rz; j++)
+				avail[j] += allocated[t][j];
+		}
+		else
+			P[++d] = P[i];
+	}
+	if(d != -1) 
+	{
+		pno = d+1;
+		goto A;
+	}
+	printf("\t< ");
+	for(i=0; i<k; i++) 
+		printf("P[%d] ", output[i]);
+	printf(">");
+}
+
+/*
+Output:
+
+
+Enter the no. of resources: 1
+
+Enter the max instances of each resources
+a = 20
+
+Enter no. of processes: 5
+
+Enter the allocation matrix
+a
+P[0]-2
+P[1]-6
+P[2]-8
+P[3]-4
+P[4]-4
+
+Enter the MAX matrix
+a
+P[0]-20
+P[1]-18
+P[2]-2
+P[3]-12
+P[4]-6
+		<P[2] P[4] P[3] P[1] P[0]>
+*/
