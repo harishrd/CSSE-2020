a456ffde2b4785409c858e9b6eb5ac70c9eb98e5
20121A1566@MNSDOMAIN.COM <20121A1566@MNSDOMAIN.COM>
2022-04-01 11:22:46 +0530

    2-1

--- Archives/1-2/OOPJ/Addition.java
+++
@@ -1,7 +0,0 @@
-package P1;
-
-public class Addition {
-	public void add(int a, int b) {
-		System.out.println(a+b);
-	}
-}
--- Archives/1-2/OOPJ/Subtraction.java
+++
@@ -1,7 +0,0 @@
-package P1;
-
-public class Subtraction {
-	public void sub(int a, int b) {
-		System.out.println(a-b);
-	}
-}
